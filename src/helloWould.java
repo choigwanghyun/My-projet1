54b9b53c7b53a81ffc4e36fc901020798296ba31
choigwanghyun <32700618+choigwanghyun@users.noreply.github.com>
2017-10-31 17:28:19 +0900

    Update helloWould.java

--- src/helloWould.java
+++ src/helloWould.java
@@ -3,13 +3,7 @@ public class helloWould {
 	public static void main(String[]args) {
 		System.out.println("i'm your father");
 		System.out.println("My star");
-	}
-
-}
-public class helloWould {
-	public static void main(String[]args) {
-int x = 0;
-System.out.println(x*100);
+		System.out.println("gogo");
 	}
 
 }
